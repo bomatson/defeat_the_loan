61f462e33caf7db562a36c7a8ebf8a487bb2a5ee
Bobby Matson <bobby@carbonfive.com>
2017-06-18 16:15:18 -0400

    update page controller spec

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule DefeatTheLoan.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Defeat the Loan"
   end
 end
